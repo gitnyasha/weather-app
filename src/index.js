a3617f2667c799fca229be58d48d7ac794d9c2c0
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-06-06 17:03:07 +0200

    fixed linter issues

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
-import { Weather } from "./weather";
-import { Dom } from "./views";
+import { Weather } from './weather';
+import { Dom } from './views';
 
-const weather = new Weather("Harare");
+const weather = new Weather('Harare');
 const dom = new Dom();
 
-document.getElementById("w-change-btn").addEventListener("click", (e) => {
-  const city = document.getElementById("city").value;
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
 
   weather.changeLocation(city);
 
-  document.getElementById("city").value = "";
+  document.getElementById('city').value = '';
 
   getWeather();
 });
 
-document.addEventListener("DOMContentLoaded", getWeather);
+document.addEventListener('DOMContentLoaded', getWeather);
 
 function getWeather() {
-  weather
-    .getWeather()
-    .then((results) => {
-      console.log(results);
-      dom.show(results);
-    })
+  weather.getWeather().then((results) => {
+    console.log(results);
+    dom.show(results);
+  })
     .catch((err) => console.log(err));
 }
--- src/views.js
+++ src/views.js
@@ -1,11 +1,11 @@
 class Dom {
   constructor() {
-    this.location = document.getElementById("w-location");
-    this.desc = document.getElementById("w-desc");
-    this.tempc = document.getElementById("w-temp-c");
-    this.tempf = document.getElementById("w-temp-f");
-    this.icon = document.getElementById("w-icon");
-    this.humidity = document.getElementById("w-humidity");
+    this.location = document.getElementById('w-location');
+    this.desc = document.getElementById('w-desc');
+    this.tempc = document.getElementById('w-temp-c');
+    this.tempf = document.getElementById('w-temp-f');
+    this.icon = document.getElementById('w-icon');
+    this.humidity = document.getElementById('w-humidity');
   }
 
   changeToC(celsius) {
@@ -21,18 +21,18 @@ class Dom {
     this.tempc.innerHTML = `${this.changeToC(weather.current.temperature)}&deg`;
     this.tempf.innerHTML = `${this.changeToF(weather.current.temperature)}&deg`;
     this.desc.textContent = weather.current.weather_descriptions[0];
-    this.icon.setAttribute("src", `${weather.current.weather_icons[0]}`);
+    this.icon.setAttribute('src', `${weather.current.weather_icons[0]}`);
     this.humidity.innerHTML = `Humidity: ${weather.current.humidity}<span>%</span>`;
-    document.getElementById("w-temp-f").style.display = "none";
-    document.getElementById("toggle").addEventListener("click", function () {
-      if (document.getElementById("switch").innerHTML === "Celsius") {
-        document.getElementById("switch").innerHTML = "Fahrenheit";
-        document.getElementById("w-temp-c").style.display = "none";
-        document.getElementById("w-temp-f").style.display = "block";
-      } else if (document.getElementById("switch").innerHTML === "Fahrenheit") {
-        document.getElementById("switch").innerHTML = "Celsius";
-        document.getElementById("w-temp-f").style.display = "none";
-        document.getElementById("w-temp-c").style.display = "block";
+    document.getElementById('w-temp-f').style.display = 'none';
+    document.getElementById('toggle').addEventListener('click', function () {
+      if (document.getElementById('switch').innerHTML === 'Celsius') {
+        document.getElementById('switch').innerHTML = 'Fahrenheit';
+        document.getElementById('w-temp-c').style.display = 'none';
+        document.getElementById('w-temp-f').style.display = 'block';
+      } else if (document.getElementById('switch').innerHTML === 'Fahrenheit') {
+        document.getElementById('switch').innerHTML = 'Celsius';
+        document.getElementById('w-temp-f').style.display = 'none';
+        document.getElementById('w-temp-c').style.display = 'block';
       }
     });
   }
