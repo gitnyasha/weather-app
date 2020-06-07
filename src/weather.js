22fcda7305e29d2bf49fae7e6d5fb2d286bef33a
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-06-07 12:26:19 +0200

    http issue

--- src/weather.js
+++ src/weather.js
@@ -1,14 +1,19 @@
 class Weather {
   constructor(city) {
-    this.apiKey = 'd05c6d708985ea91c4e8f243752d95c0';
+    this.apiKey = "d05c6d708985ea91c4e8f243752d95c0";
     this.city = city;
   }
 
   async getWeather() {
-    const response = await fetch(
-      `https://api.weatherstack.com/current?access_key=${this.apiKey}&query=${this.city}`,
-    );
-
+    if (location.protocol === "http:") {
+      const response = await fetch(
+        `http://api.weatherstack.com/current?access_key=${this.apiKey}&query=${this.city}`
+      );
+    } else {
+      const response = await fetch(
+        `https://api.weatherstack.com/current?access_key=${this.apiKey}&query=${this.city}`
+      );
+    }
     const responseData = await response.json();
 
     return responseData;
