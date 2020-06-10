07c7c817d23255144d83ff78f9be0a6a2f498da7
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-06-10 15:20:15 +0200

    lintor issue

--- src/views.js
+++ src/views.js
@@ -11,12 +11,12 @@ class Dom {
     this.locate.innerHTML = `${weather.name}`;
     this.temp.innerHTML = `${Math.floor(weather.main.temp - 273.15)}&deg C`;
     this.desc.innerHTML = `${weather.weather[0].description}`;
-    this.icon.setAttribute('src', `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`);
+    this.icon.setAttribute("src", `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`);
     this.humidity.innerHTML = `Humidity: ${weather.main.humidity}<span>%</span>`;
-    document.getElementById('toggle').addEventListener('click', () => {
+    document.getElementById("toggle").addEventListener("click", () => {
       this.temp.innerHTML = `${Math.floor((weather.main.temp - 273.15) * 9 / 5 + 32)}&deg F`;
     });
-    document.getElementById('togglef').addEventListener('click', () => {
+    document.getElementById("togglef").addEventListener("click", () => {
       this.temp.innerHTML = `${Math.floor(weather.main.temp - 273.15)}&deg C`;
     });
   }
